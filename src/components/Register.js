6cfd4d22eb05c0a152fe867ab738088300655439
ahtisham313 <130073639+ahtisham313@users.noreply.github.com>
2023-06-23 08:38:12 +0500

    Somethinjg

--- src/components/Register.js
+++ src/components/Register.js
@@ -71,4 +71,97 @@ const Register = () => {
   );
 };
 
+const styles = {
+  container: {
+    maxWidth: "400px",
+    margin: "0 auto",
+    padding: "20px",
+  },
+  title: {
+    fontSize: "24px",
+    fontWeight: "bold",
+    marginBottom: "20px",
+    textAlign: "center",
+  },
+  input: {
+    width: "100%",
+    padding: "10px",
+    marginBottom: "10px",
+  },
+  button: {
+    width: "100%",
+    padding: "10px",
+    backgroundColor: "#007BFF",
+    color: "#FFF",
+    border: "none",
+    borderRadius: "5px",
+    cursor: "pointer",
+  },
+};
+
 export default Register;
+
+/*import React, { useEffect, useState } from "react"
+import axios from "axios"
+import { useNavigate, Link } from "react-router-dom"
+
+
+function Login() {
+    const history=useNavigate();
+
+    const [email,setEmail]=useState('')
+    const [password,setPassword]=useState('')
+
+    async function submit(e){
+        e.preventDefault();
+
+        try{
+
+            await axios.post("http://localhost:8000/signup",{
+                email,password
+            })
+            .then(res=>{
+                if(res.data=="exist"){
+                    alert("User already exists")
+                }
+                else if(res.data=="notexist"){
+                    history("/home",{state:{id:email}})
+                }
+            })
+            .catch(e=>{
+                alert("wrong details")
+                console.log(e);
+            })
+
+        }
+        catch(e){
+            console.log(e);
+
+        }
+
+    }
+
+
+    return (
+        <div className="login">
+
+            <h1>Signup</h1>
+
+            <form action="POST">
+                <input type="email" onChange={(e) => { setEmail(e.target.value) }} placeholder="Email"  />
+                <input type="password" onChange={(e) => { setPassword(e.target.value) }} placeholder="Password" />
+                <input type="submit" onClick={submit} />
+
+            </form>
+
+            <br />
+            <p>OR</p>
+            <br />
+
+            <Link to="/">Login Page</Link>
+
+        </div>
+    )
+}
+
+export default Register;*/
